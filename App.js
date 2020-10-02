07070dfd6a33156051efc2b7e15700404a87fcb1
Atharv Andewar <atharvandewar1010@gmail.com>
2020-10-03 03:32:42 +0530

    second commit

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 
   return(
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Feeds" >
+      <Stack.Navigator initialRouteName="Main" >
         <Stack.Screen name="Main" component={Main} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         <Stack.Screen name="Signup" component={Signup} options={{ headerShown: false }} />
